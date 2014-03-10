d20944ebffa3523be5879908ad1e64e749bd6133
Patrick McConlogue <patrick.mcconlogue@gooddata.com>
2014-03-10 12:02:32 -0700

    - info.json -> Added created, author, description, and is_live fields. - app_builder.rb -> Generates index page and index.html - app_builder.rb -> Only builds apps with is_live fields. - index.html -> Shows author, version, description, and title of available apps.

--- web/render/app_builder.rb
+++ web/render/app_builder.rb
@@ -9,71 +9,103 @@ require 'pry'
 class AppBuilder
     
     @@apps = []
+    @@apps_loaded = 0
 
-    def render(app_page)
+    def renderIndex(apps)
+      @apps = apps
+      begin
+        if File.directory?('./pages')
+          header = IO.read('./templates/header.html')
+          footer = IO.read('./templates/footer.html')
+          page = File.open("./pages/index.html", "w+")            
+          page.puts header
+          page.puts '<div class="hero-unit">'
+          page.puts '<h1>Ruby App Executors</h1><p>Packable apps to extend the functionality of your Good Data project or <a href="#"> build your own.</a></p>'
+          page.puts '</div>'
+          page.puts '<div align="center" class="container"><ul class="thumbnails">'
+          @apps.each do |app|
+            app_name = app['name'].split('_').map(&:capitalize).join(' ')
+            app_url = app['name']+'.html'
+            page.puts "<li class=\"span4\"><a class=\"thumbnail\" href=\"#{app_url}\"><h4>#{app_name}</h4>"
+            page.puts "<p>#{app['description']}</p>"
+            page.puts "<small><div style=\"align:right\">#{app['author']['name']} | <cite title=\"#{app['version']}\">#{app['version']}</cite></div></small>"
+            page.puts "</a></li>"
+          end          
+          page.puts '</ul></div>'
+          page.puts footer
+
+        else
+          Dir.mkdir('pages')
+          renderIndex(@apps)
+        end
+      rescue IOError => e
+      ensure
+        page.close unless page == nil
+      end
+    end
+
+    def renderApp(app_page)
       @app_page = app_page
       @app_name = app_page['name']
       @app_version = app_page['version']
       @app_category = app_page['category']
       @app_language = app_page['language']
       @app_tags = app_page['tags']
+      @app_description = app_page['description']
       @parameters = app_page['parameters']
-      
+
       begin
         if File.directory?('./pages')
           header = IO.read('./templates/header.html')
           footer = IO.read('./templates/footer.html')
-          appPage = File.open("./pages/"+@app_name+".html", "w+")            
-          appPage.puts header
-          appPage.puts '<p align="center" class="small"> <kbd>Version '+@app_version+'</kbd></p>'
-          appPage.puts '<h1 class="cover-heading">'+@app_name.split('_').map(&:capitalize).join(' ')+'</h1>'
-          appPage.puts '<p class="lead">'+@app_page["tags"]+'.</p>'
-
-          appPage.puts "<h3>Description</h3>"
-          appPage.puts "<div>" + @app_page["description"] + "</div>"
-
-          appPage.puts "<h3>Parameters</h3>"
-          appPage.puts '<table id="details" style="width:500px;" align="center" class="table table-condensed">'
-          appPage.puts '<tr><td align="left">Name</td><td align="left">Description</td><td>Mandatory</td></tr>'
+          page = File.open("./pages/"+@app_name+".html", "w+")            
+          page.puts header
+          page.puts '<div align="center" class="container">'
+          page.puts '<p align="center" class="small"> <kbd>Version '+@app_version+'</kbd></p>'
+          page.puts '<h1 class="cover-heading">'+@app_name.split('_').map(&:capitalize).join(' ')+'</h1>'
+          page.puts '<p class="lead">'+@app_page["tags"]+'.</p>'
+          page.puts "<h3>Description</h3>"
+          page.puts '<p align="center">'+@app_description+'</p>'
+          page.puts "<h3>Parameters</h3>"
+          page.puts '<table id="details" style="width:500px;" align="center" class="table table-condensed">'
+          page.puts '<tr><td align="left">Name</td><td align="left">Description</td><td>Mandatory</td></tr>'
           @parameters.each do |param|
-            appPage.puts '<tr><td align="left">'+param["name"]+'</td><td align="left">'+param["description"]+'</td><td>' + (param["mandatory"] || false).to_s + '</td></tr>'
+            page.puts '<tr><td align="left">'+param["name"]+'</td><td align="left">'+param["description"]+'</td><td>' + (param["mandatory"] || false).to_s + '</td></tr>'
           end          
-          appPage.puts '</table>'
-          
-          appPage.puts '<p class="lead">Run instructions</p>'
-          appPage.puts '<pre>'
-          appPage.puts 'cd app_store/apps'
-          appPage.puts "bin/gooddata -Ulogin -Ppass -p PROJECT_PID run_ruby --dir #{@app_name}/ --params #{@app_name}/params.json  --name \"Download objects execution\" --remote"
-          appPage.puts '</pre>'
-          
-          appPage.puts '<p class="lead">'+@app_name+'</p>' #more text
-          appPage.puts '<a href="'+@app_name+'" class="btn btn-lg btn-default">Install</a></div>' # uri or rel API call. 
-          
-          
-          appPage.puts footer
+          page.puts '</table>'
+          page.puts '<p class="lead">Run instructions</p>'
+          page.puts '<pre>'
+          page.puts 'cd app_store/apps'
+          page.puts "bin/gooddata -Ulogin -Ppass -p PROJECT_PID run_ruby --dir #{@app_name}/ --params #{@app_name}/params.json  --name \"Download objects execution\" --remote"
+          page.puts '</pre>'
+          page.puts '<p class="lead">'+@app_name+'</p>' 
+          page.puts '<a href="'+@app_name+'" class="btn btn-lg btn-default">Install</a></div>'
+          page.puts '</div'
+          page.puts footer
+          @@apps_loaded += 1 
         else
           Dir.mkdir('pages')
-          render(@app_page)
+          renderApp(@app_page)
         end
       rescue IOError => e
       ensure
-        appPage.close unless appPage == nil
+        page.close unless page == nil
       end
     end
 
-    def build  
-      dirs = Dir.entries("../../apps").drop(2)
-      dirs.each { |dir| 
-        app = JSON.parse( IO.read('../../apps/'+dir+'/info.json'))
-        @@apps.push(app)
+    def build 
+      Dir.glob("../../apps/*/info.json") do |path| 
+        app = JSON.parse( File.read(path))
+        if app['is_live']
+          @@apps << app
+        end
+      end
+      puts "Apps Found: #{@@apps.length}"
+      @@apps.each{ |app|         
+        renderApp(app)
+        puts "=> App Published: #{app['name']}"
       }
-
-      puts "Apps Loaded: "+@@apps.length.to_s
-      @@apps.each{ |app| 
-       render(app)        
-      }
-      # render(@@apps[0])
-
+      renderIndex(@@apps)
     end
     def findApps      
       apps = []
