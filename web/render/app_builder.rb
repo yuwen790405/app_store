697945c8aca1eedee3b927c603803b283380ca5d
Tomas Svarovsky <svarovsky.tomas@gmail.com>
2014-03-10 14:51:01 +0100

    Committed better versions of info.json s. Tweaked renderer a bit.

--- web/render/app_builder.rb
+++ web/render/app_builder.rb
@@ -1,9 +1,10 @@
 # Renders HTML page of all apps bas on 
 require 'rubygems' 
-require 'bundler/setup' 
+# require 'bundler/setup' 
 require 'pp'
 require 'json'
 require 'find'
+require 'pry'
 
 class AppBuilder
     
@@ -16,13 +17,8 @@ class AppBuilder
       @app_category = app_page['category']
       @app_language = app_page['language']
       @app_tags = app_page['tags']
-      @app_token = app_page['parameters'][0]['name']
-      @app_token_description = app_page['parameters'][0]['description']
-      @app_uri = app_page['parameters'][1]['name']
-      @app_uri_description = app_page['parameters'][1]['description']
-      @app_spec = app_page['parameters'][2]['name']
-      @app_spec_description = app_page['parameters'][2]['description']
-
+      @parameters = app_page['parameters']
+      
       begin
         if File.directory?('./pages')
           header = IO.read('./templates/header.html')
@@ -31,14 +27,29 @@ class AppBuilder
           appPage.puts header
           appPage.puts '<p align="center" class="small"> <kbd>Version '+@app_version+'</kbd></p>'
           appPage.puts '<h1 class="cover-heading">'+@app_name.split('_').map(&:capitalize).join(' ')+'</h1>'
-          appPage.puts '<p class="lead">'+@app_category.split('_').map(&:capitalize).join(' ')+', built lovingly in '+@app_language.capitalize+'.</p>' #description
+          appPage.puts '<p class="lead">'+@app_page["tags"]+'.</p>'
+
+          appPage.puts "<h3>Description</h3>"
+          appPage.puts "<div>" + @app_page["description"] + "</div>"
+
+          appPage.puts "<h3>Parameters</h3>"
           appPage.puts '<table id="details" style="width:500px;" align="center" class="table table-condensed">'
-          appPage.puts '<tr><td align="left">'+@app_token.split('_').map(&:capitalize).join(' ')+'</td><td align="left">'+@app_token_description+'</td></tr>'
-          appPage.puts '<tr><td align="left">'+@app_uri.split('_').map(&:capitalize).join(' ')+'</td><td align="left">'+@app_uri_description+'</td></tr>'
-          appPage.puts '<tr><td align="left">'+@app_spec.split('_').map(&:capitalize).join(' ')+'</td><td align="left">'+@app_spec_description+'</td></tr>'
+          appPage.puts '<tr><td align="left">Name</td><td align="left">Description</td><td>Mandatory</td></tr>'
+          @parameters.each do |param|
+            appPage.puts '<tr><td align="left">'+param["name"]+'</td><td align="left">'+param["description"]+'</td><td>' + (param["mandatory"] || false).to_s + '</td></tr>'
+          end          
           appPage.puts '</table>'
+          
+          appPage.puts '<p class="lead">Run instructions</p>'
+          appPage.puts '<pre>'
+          appPage.puts 'cd app_store/apps'
+          appPage.puts "bin/gooddata -Ulogin -Ppass -p PROJECT_PID run_ruby --dir #{@app_name}/ --params #{@app_name}/params.json  --name \"Download objects execution\" --remote"
+          appPage.puts '</pre>'
+          
           appPage.puts '<p class="lead">'+@app_name+'</p>' #more text
           appPage.puts '<a href="'+@app_name+'" class="btn btn-lg btn-default">Install</a></div>' # uri or rel API call. 
+          
+          
           appPage.puts footer
         else
           Dir.mkdir('pages')
@@ -58,12 +69,10 @@ class AppBuilder
       }
 
       puts "Apps Loaded: "+@@apps.length.to_s
-
-      #@@apps.each{ |app| 
-      #  render(app)
-        
-      #}
-      render(@@apps[0])
+      @@apps.each{ |app| 
+       render(app)        
+      }
+      # render(@@apps[0])
 
     end
     def findApps      
