d27f8e3a4f9fd9fddb00b8dd8b7d5d9c0f662674
Patrick McConlogue <patrick.mcconlogue@gooddata.com>
2014-06-18 11:58:33 -0700

    local brick changes

--- apps/user_brick/spec.rb
+++ apps/user_brick/spec.rb
@@ -4,20 +4,18 @@ require 'rspec'
 
 describe GoodData::Bricks::UserBrick do
 
-
   it 'should change the default column from role to privileges' do
 
     GoodData.logging_on
 
-    GoodData.connect('svarovsky+gem_tester@gooddata.com','jindrisska')
+    GoodData.connect('svarovsky+gem_tester@gooddata.com', 'jindrisska')
 
-    # REPLACE with GIST
-    #params = {
-    #    :domain => "NO",
-    #    :project => "NO",
-    #    :csv_path => "NO"
-    #}
-    
+
+    params = {
+        :domain => "gooddata-tomas-svarovsky",
+        :project => "pua4g8eplv06iayn5nkz0brqnb5l7i70",
+        :csv_path => "./demo.csv"
+    }
 
     GoodData::Bricks::UserBrick.new().call(params)
 
