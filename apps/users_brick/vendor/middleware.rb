9c308730b7055d3955ecb66d0a7f8af3a4fc8af6
Tomas Svarovsky <svarovsky@gooddata.com>
2015-02-26 22:24:56 -0800

    Fixing GoodDataMiddleware

--- apps/users_brick/vendor/middleware.rb
+++ apps/users_brick/vendor/middleware.rb
@@ -97,3 +97,34 @@ module GoodData
     end
   end
 end
+
+module GoodData
+  module Bricks
+    class GoodDataMiddleware < Bricks::Middleware
+      def call(params)
+        logger = params['GDC_LOGGER']
+        token_name = 'GDC_SST'
+        protocol_name = 'GDC_PROTOCOL'
+        server_name = 'GDC_HOSTNAME'
+        project_id = params['GDC_PROJECT_ID']
+
+        fail 'SST (SuperSecureToken) not present in params' if params[token_name].nil?
+
+        server = if !params[protocol_name].empty? && !params[server_name].empty?
+                   "#{params[protocol_name]}://#{params[server_name]}"
+                 end
+
+        client = if params['GDC_USERNAME'].nil? || params['GDC_PASSWORD'].nil?
+                   GoodData.connect(sst_token: params['GDC_SST'], server: server)
+                 else
+                   GoodData.connect(params['GDC_USERNAME'], params['GDC_PASSWORD'], server: server)
+                 end
+        GoodData.logger = logger
+        @app.call(params.merge({
+          'GDC_GD_CLIENT' => client,
+          'gdc_project' => client.projects(project_id)
+        })
+      end
+    end
+  end
+end
